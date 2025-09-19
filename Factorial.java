7573fb3d993ee8b9aff34ac9cfdacaa4fe742d8e
tusharkr02p <tushar033531@gmail.com>
2025-09-19 18:55:40 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    
+    public static int facto(int n){
+        int f= 1; 
+        for(int i = 1 ; i <= n ; i++ ) f = f * i;
+        return f;
+    }
+    public static void main(String[] args) {
+        System.out.println(facto(4));
+    }
+    
+}

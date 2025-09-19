39ad4bfdf3559409d03da9c3976ede69314b2ae5
tusharkr02p <tushar033531@gmail.com>
2025-09-19 19:28:45 +0530

    Function Overloading

---
+++ Functions/Function_overloading.java
@@ -0,0 +1,13 @@
+public class Function_overloading {
+    public static int sum(int a , int b){
+        return a + b;
+    }
+
+    public static int sum(int a , int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(1,2));
+    }
+    
+}

47c0c2dc417488aac4a752c877973d132e028a13
tusharkr02p <tushar033531@gmail.com>
2025-09-20 12:01:51 +0530

    Prime Optimised

--- Functions/Prime_or_not.java
+++ Functions/Prime_or_not.java
@@ -1,16 +1,20 @@
 public class Prime_or_not {
 
     public static boolean isPrime(int n){
-        boolean isPrime = true;
+        if(n ==2) return true;
+        // boolean isPrime = true;
         for(int i = 2 ; i <= n-1; i++){
-            if( n % i == 0) {isPrime =false ;
-                break;
+            if( n % i == 0) {
+            // isPrime =false ;
+            return false;
+                // break;
             }
             
         }
-        return  isPrime;
+        // return  isPrime;
+        return true;
     }
  public static void main(String[] args) {
-    System.out.println(isPrime(6));
+    System.out.println(isPrime(2));
  }   
 }
